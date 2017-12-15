0f1a8b749c556c2c248509c9bda0ad594e119b41
zxvnme <igoranonim1337@gmail.com>
2017-12-15 22:58:37 +0100

    📚 Theme changes part (2 of 3)
    - Added particles to mobile.html.
    - Changed major site colors.
    - Renamed some files.

--- resources/js/app.js
+++
@@ -1,3 +0,0 @@
-particlesJS.load('particles-js', 'resources/js/configs/particles.json', function () {
-    console.log('particles.js loaded - callback');
-});
---
+++ resources/js/index.js
@@ -0,0 +1,3 @@
+particlesJS.load('particles-js', 'resources/js/configs/particles-index.json', function () {
+    console.log('particles.js main config loaded - callback');
+});
